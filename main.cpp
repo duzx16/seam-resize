429ecbb82b8df1ebe503ece605c6c22b264164ca
duzx <duzx16@mails.tsinghua.edu.cn>
2018-06-23 20:21:26 +0800

    恢复区域保护和删除

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 
 int main()
 {
-    cv::Mat img = cv::imread("/Users/Duzx/Downloads/5.jpg"), seam_img;
+    cv::Mat img = cv::imread("/Users/Duzx/Downloads/input.jpg"), seam_img;
     cv::Mat mask(img.rows, img.cols, CV_64F, 0.0);
     cv::Mat mask_img = cv::imread("/Users/Duzx/Downloads/mask.jpg");
     for (int i = 0; i < mask_img.rows; ++i)
@@ -17,9 +17,9 @@ int main()
             if (mask_img.at<Vec3b>(i, j)[0] <= 3 and mask_img.at<Vec3b>(i, j)[1] <= 3 and
                 mask_img.at<Vec3b>(i, j)[2] >= 250)
                 mask.at<double>(i, j) = -1e50;
-            //if (mask_img.at<Vec3b>(i, j)[1] <= 3 and mask_img.at<Vec3b>(i, j)[2] <= 3 and
-            //    mask_img.at<Vec3b>(i, j)[0] >= 250)
-            //    mask.at<double>(i, j) = 1e50;
+            if (mask_img.at<Vec3b>(i, j)[1] <= 3 and mask_img.at<Vec3b>(i, j)[2] <= 3 and
+                mask_img.at<Vec3b>(i, j)[0] >= 250)
+                mask.at<double>(i, j) = 1e50;
         }
     }
     shrink_img(img, seam_img, 0.2, 0.2, mask);
